2ee846e26cd903d74e3f6de02a3116c67a3a361e
divya <97022799+divya220@users.noreply.github.com>
2024-08-04 15:29:51 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello{
+    public static void main(String args[]){
+        int x = 10;
+        int y = 25;
+        int z = x + y;
+        System.out.println("Sum of x and y:-"+ z);
+    }
+}
